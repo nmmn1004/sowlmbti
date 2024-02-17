f917b287014caf5b742dbce3399d51faf010b245
nmmn <51752838+nmmn1004@users.noreply.github.com>
2024-02-17 19:02:26 +0900

    image update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import '../style/home.css';
 import { Link } from "react-router-dom";
-
+import HomeImg from "../img/homeImage.png";
 export default function Home() {
     return (
         <div className='homeContainer'>
             {/* <div className='homeTitle'>
                 부엉이 성격유형검사
             </div> */}
-            <div className="homeRandomImage">
-                랜덤 이미지
-            </div>
+            <picture className="homeRandomImage">
+                <img className='homeImage' src={HomeImg}></img>
+            </picture>
             <Link className='homeLink' to="/selection">시작하기</Link>
         </div>
     );
